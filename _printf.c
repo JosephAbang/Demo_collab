aafb3453f8aa1bcfea237f590e741d82a6382841
Joseph <abangjosef@gmail.com>
2023-03-28 16:52:57 -0700

    Print format specifiers s,c,%

---
+++ _printf.c
@@ -0,0 +1,82 @@
+#include "main.h"
+/**
+ * get_match_func - match format with function format
+ * @format: It's a character string
+ * @list: the list of arguments the function _printf is receiving
+ * Return: the number of character the function is printing
+ */
+int get_match_func(const char *format, va_list list, st_fmt st_format[])
+{
+	int count = 0, i = 0, j, k = 0, num = 0;
+
+	for (i = 0; format && format[i] != 0; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			count += 1;
+		}
+		else
+		{
+			for (j = 0; st_format[j].fmt; j++)
+			{
+				if (format[i + 1] == st_format[j].fmt[k])
+				{
+					num = st_format[j].func(list);
+					count += num;
+					i++;
+					break;
+				}
+			}
+			if (st_format[j].fmt == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != 0)
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					count += 2;
+					i++;
+				}
+				else
+					return (-1);
+			}
+		}
+	}
+	if (format == NULL)
+		return (-1);
+	return (count);
+
+
+}
+
+/**
+ * _printf - print a char or a string
+ * @format: It's a character string
+ * Return: the number of character the function is printing
+ */
+int _printf(const char *format, ...)
+{
+	st_fmt st_format[] = {
+		{"c", func_char},
+		{"s", func_string},
+		{"d", func_digit},
+		{"i", func_digit},
+		{"%", func_percent},
+		{"b", func_binary_convert},
+		{"u", func_unsig_int},
+		{"o", func_octal_convert},
+		{"x", func_hex_Lowcase_convert},
+		{"X", func_hex_Upcase_convert},
+		{"S", func_stringUppercase},
+		{"r", func_revstr},
+		{"R", func_rot13},
+		{NULL, NULL}};
+
+	va_list list;
+	int count = 0;
+
+	va_start(list, format);
+	count =	get_match_func(format, list, st_format);
+	va_end(list);
+	return (count);
+}
